5ddf1229c3401a6a96140c7ab006254fac5ca4c0
tobia-codes <tobi96.anton@gmail.com>
2026-05-04 00:37:11 +0200

    fixes script issues

--- scripts/apply-tag.sh
+++ scripts/apply-tag.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-# This script updates the version number for
-# the addon based on the current commit timestamp
+# Dieses Skript aktualisiert die Version in der config.yaml
+# basierend auf dem aktuellen Git-Zeitstempel/Hash
+
 TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
-sed -i "s/version:.*/version: \"$TAG_NAME\"/" addon/config.yaml
+
+# Wir suchen nach der Zeile, die mit 'version:' beginnt, 
+# und ersetzen alles dahinter durch den neuen TAG_NAME in Anführungszeichen.
+sed -i "s/^version:.*/version: \"$TAG_NAME\"/" addon/config.yaml
